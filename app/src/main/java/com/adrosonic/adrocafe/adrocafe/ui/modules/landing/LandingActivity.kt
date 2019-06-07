087efbbf699f06b81af32f29ff651bdffb364002
Prathamesh More <prathamesh.s.more@gmail.com>
2019-06-07 16:24:49 +0530

    deleted snackbar finally

--- app/src/main/java/com/adrosonic/adrocafe/adrocafe/ui/modules/landing/LandingActivity.kt
+++ app/src/main/java/com/adrosonic/adrocafe/adrocafe/ui/modules/landing/LandingActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.core.view.GravityCompat
 import androidx.appcompat.app.ActionBarDrawerToggle
